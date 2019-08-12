aed49913f657d660a2a91b8d541a3d223418db2b
Ahmad Zohery <AhmadZohery@gmail.com>
2019-08-12 06:59:03 +0200

    custom edit navbar

--- Blogger/urls.py
+++ Blogger/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('', include('user.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('blog.urls', namespace='blog')),
+    path('', include('user.urls', namespace='user')),
 ]
